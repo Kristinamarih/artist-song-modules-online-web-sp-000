83f717b126b0f9b5c9ece10997cfab40663df876
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-23 10:26:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable::ClassMethods
+  extend Findable
   include Paramble
   attr_accessor :name
   attr_reader :songs
