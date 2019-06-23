e3b193c6068c7af0e3dcab8b2bd47ebaa7421884
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-23 10:26:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable::ClassMethods
+  extend Findable
   include Paramble
   attr_accessor :name
   attr_reader :artist
